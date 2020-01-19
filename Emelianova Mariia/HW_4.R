d7f384da6e79bb9dabddd9ee6b8c436ea3350b2b
Masha <not_like_that@mail.ru>
2020-01-19 16:08:10 +0300

    Update HW_4.R

--- Emelianova Mariia/HW_4.R
+++ Emelianova Mariia/HW_4.R
@@ -15,18 +15,30 @@ weather$date <- as.Date(weather$date)
 # Then we spread the dataset by all measures
 weather <- pivot_wider(weather, id_cols = date, names_from = measure, values_from = some_data)
 
-weather <- as.data.frame(weather)
-
 
 # We also want to avoid spare spaces and NA's in the columns. To do that we:
 
-# 1.Replace empty spaces in "Events" column with "No event"
+# 1. Replace empty spaces in "Events" column with "No event"
 weather$Events[weather$Events == ""] <- "No event"
 
-# 2. Replace NA's in numeric columns with mean values
+# 2. Replace "T" in PrecipitationIn column with 0.00
+weather$PrecipitationIn[weather$PrecipitationIn == "T"] <- replace(weather$PrecipitationIn, 
+                                                                   weather$PrecipitationIn == "T", 
+                                                                   format(0, nsmall = 2))
+
+# 3. Replace NA's in numeric columns with mean values
 for (i in c(2:19, 21, 23)){
-  weather[ , i] <- replace(weather[ , i], which(is.character(weather[ , i])), as.numeric(weather[ , i], na.rm=TRUE))}
+  weather[ , i] <- replace(weather[ , i], 
+                           which(is.character(weather[ , i])), 
+                           as.numeric(weather[ , i], na.rm=TRUE))}
 
 for (i in c(2:19, 21, 23)){
-  weather[ , i] <- replace(weather[ , i], which(is.na(weather[ , i])), mean(weather[ , i], na.rm=TRUE))}
-  
+  weather[ , i] <- replace(weather[ , i], 
+                           which(is.na(weather[ , i])), 
+                           mean(weather[ , i], na.rm=TRUE))}
+
+
+# Then we change data type to numeric in 'numeric' columns
+weather[ , c(2:21, 23)] <- sapply(weather[ , c(2:21, 23)], as.numeric) 
+
+str(weather)
